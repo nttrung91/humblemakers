6f94ad10a08d7d15b01c34633eae50fb4cdc7833
Kubi <trungn3@uci.edu>
2014-04-21 13:42:49 -0700

    Modified the lightbox and add img to the page

--- js/global.js
+++ js/global.js
@@ -1,1 +1,19 @@
-$( 'a' ).imageLightbox();
+var overlayOn = function()
+      {
+        $( '<div id="imagelightbox-overlay"></div>' ).appendTo( 'body' );
+      },
+      overlayOff = function()
+      {
+        $( '#imagelightbox-overlay' ).remove();
+      };
+
+
+$( '.galleryLink' ).imageLightbox(
+    {
+      onStart:   function() { overlayOn(); },
+      onEnd:     function() { overlayOff();}
+    });
+
+
+
+        // $( '.galleryLink' ).imageLightbox();
